cba8e54d17f2539e600d74ec290667324b449577
sfmhrn30 <safam301008@gmail.com>
2024-07-18 21:19:41 +0530

    4- prepared app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-//import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -22,6 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 //midddle lyers
 app.use(express.json({ limit:"10mb" })); //reg fn that runs between request and respnse
@@ -34,6 +35,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",NotificationionRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
